ce97efbf5d2aab55280e5122e28ef34e7d4ce7be
John Andrew Moquia <andrewmoquia09@gmail.com>
2025-01-15 12:54:55 +0800

    fix: rename main application

--- src/main/java/com/moqs/demo/Application.java
+++ src/main/java/com/moqs/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootTutorialApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootTutorialApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
