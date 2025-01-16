fc210be75731d6be1594300e95369fd33992662f
John Andrew Moquia <andrewmoquia09@gmail.com>
2025-01-16 16:22:22 +0800

    feat: create sql files

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Run (
+    id INT NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    started_on timestamp NOT NULL,
+    ended_on timestamp NOT NULL,
+    miles INT NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
